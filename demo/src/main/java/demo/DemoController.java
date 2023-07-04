70eff092360bc7adac414da5d9607759ef7440c0
Azeez18 <138334984+Azeez18@users.noreply.github.com>
2023-07-04 11:39:09 -0500

    Post
    Post

--- demo/src/main/java/demo/DemoController.java
+++ demo/src/main/java/demo/DemoController.java
@@ -2,6 +2,9 @@ package demo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class DemoController {
 
@@ -20,8 +23,34 @@ public class DemoController {
     }
 
     @GetMapping("/new/{name}")
-    public String welcomeMessageNew(@PathVariable String name) {
-        //append URL
-        return "Hello PathVariable "+name;
+    public List<Request> welcomeMessageNew(@PathVariable String name) {
+        List<Request> responseList = new ArrayList<>();
+        Request response = new Request();
+        Request response1 = new Request();
+        response.setName(name);
+        response.setAge(30);
+        response.setCity("HYD");
+
+        response1.setName("Test"+name);
+        response1.setAge(30);
+        response1.setCity("HYD");
+        responseList.add(response1);
+        responseList.add(response);
+        return responseList;
+    }
+    // New POST endpoint
+    @PostMapping("/greeting")
+    public String createGreeting(@RequestBody String name) {
+        return "Hello RequestBody " + name;
+    }
+
+    @PostMapping("/welcome")
+    public String welcome(@RequestBody Request request) {
+        if("Hyderabad".equalsIgnoreCase(request.getCity())) {
+            request.setCity("HYD");
+        }
+        return "Hello " + request.getName() +" welcome and age is "+request.getAge()+" City "+request.getCity();
     }
 }
+
+
---
+++ demo/src/main/java/demo/Request.java
@@ -0,0 +1,33 @@
+package demo;
+
+
+public class Request {
+
+    private String name;
+    private String city;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
